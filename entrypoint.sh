d6e4b0e9bc0fef95fd2ddc4f657bef6fe87a0f50
Renaud de Chivré <renaud@tahitiwebdesign.com>
2021-02-08 16:43:28 -1000

    fix: add DB_HOST support instead of DB_ADDR in env variable
    Renamed DB_ADDR to DB_HOST for consistency (keep DB_ADDR for backward compat)
    Updated README

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 # set the postgres database host, port, user and password according to the environment
 # and pass them as arguments to the odoo process if not present in the config file
-: ${HOST:=${DB_ADDR:='db'}}
+: ${HOST:=${DB_HOST:=${DB_ADDR:='db'}}}
 : ${PORT:=${DB_PORT:=5432}}
 : ${NAME:=${DB_NAME:='odoo'}}
 : ${USER:=${DB_USER:=${POSTGRES_USER:='odoo'}}}
