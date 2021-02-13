af646568b85dd227f75bac57d84ce5662ce7c6ef
Renaud de Chivré <renaud@tahitiwebdesign.com>
2021-02-12 16:29:09 -1000

    feat: enable launching odoo in a debugger
    debugpy & pydevd debuggers are supported

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+: ${DEBUG:=${DEBUG_MODE:='none'}}
+: ${PYDEV_DEBUG_SERVER:='host.docker.internal'}
+
 # set the postgres database host, port, user and password according to the environment
 # and pass them as arguments to the odoo process if not present in the config file
 : ${HOST:=${DB_HOST:=${DB_ADDR:='db'}}}
@@ -29,23 +32,37 @@ check_config "db_name" "--database" "$NAME"
 check_config "db_user" "--db_user" "$USER"
 check_config "db_password" "--db_password" "$PASSWORD"
 
+DEBUG_ARG=""
+DEBUG_CMD=""
+case "$DEBUG" in
+    debugpy)
+        DEBUG_ARG="--debug"
+        DEBUG_CMD="python -m debugpy --listen 0.0.0.0:3001"
+        ;;
+    pydevd)
+        DEBUG_ARG="--debug"
+        DEBUG_CMD="python -m pydevd --port 3001 --client $PYDEV_DEBUG_SERVER --file"
+        ;;
+esac
+
+COMMAND=""
 case "$1" in
     -- | openerp-server)
         shift
         if [[ "$1" == "scaffold" ]] ; then
-            echo "ENTRYPOINT: exec openerp-server -c $ODOO_CFG \"$@\""
-            exec openerp-server -c $ODOO_CFG "$@"
+            COMMAND="/usr/bin/openerp-server $DEBUG_ARG -c $ODOO_CFG $@"
         else
-            echo "ENTRYPOINT: exec openerp-server -c $ODOO_CFG \"$@\" \"${DB_ARGS[@]}\""
-            exec openerp-server -c $ODOO_CFG "$@" "${DB_ARGS[@]}"
+            COMMAND="/usr/bin/openerp-server $DEBUG_ARG -c $ODOO_CFG $@ ${DB_ARGS[@]}"
         fi
         ;;
     -*)
-        echo "ENTRYPOINT: exec openerp-server -c $ODOO_CFG \"$@\" \"${DB_ARGS[@]}\""
-        exec openerp-server -c $ODOO_CFG "$@" "${DB_ARGS[@]}"
+        COMMAND="/usr/bin/openerp-server $DEBUG_ARG -c $ODOO_CFG $@ ${DB_ARGS[@]}"
         ;;
     *)
-        exec "$@"
+        COMMAND="$@"
 esac
 
-exit 1
+echo "ENTRYPOINT: exec $DEBUG_CMD $COMMAND"
+exec $DEBUG_CMD $COMMAND
+
+exit 0
